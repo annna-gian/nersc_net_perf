422d0a1716b8d97fc65bf0ca274fdc7029109779
annna-gian <34522903+annna-gian@users.noreply.github.com>
2025-01-22 18:06:01 -0800

    Update totals.py
    updated print messages

--- totals.py
+++ totals.py
@@ -26,29 +26,29 @@ def main():
     interactive = ['128.55.205.18', '128.55.205.19', '128.55.205.20', '128.55.205.21', 
                    'dtn01.nersc.gov', 'dtn02.nersc.gov', 'dtn03.nersc.gov', 'dtn04.nersc.gov']
 
-    for dtn in interactive:
-        for file in sys.argv[1:]:
-            with open(file, 'r') as f:
-                tstat_data = [json.loads(line) for line in f]
-                if '2022' in f.name:
-                    print('Its a 2022 file')
-                    for i in tstat_data:
-                        if '.nersc.gov' in i['_source']['source'] and '.nersc.gov' in i['_source']['dest']:
-                            print('EW traffic DTNs')
-                            total_ew += float(i['_source']['values']['file_size_MB'])
-                        elif '.nersc.gov' in i['_source']['source'] and '.nersc.gov' not in i['_source']['dest']:
-                            print('NS traffic-leaving NERSC')
-                            total_out_ns += float(i['_source']['values']['file_size_MB'])
+    
+            for file in sys.argv[1:]:
+                with open(file, 'r') as f:
+                    tstat_data = [json.loads(line) for line in f]
+                    if '2022' in f.name:
+                        print('Its a 2022 file')
+                        for i in tstat_data:
+                            if '.nersc.gov' in i['_source']['source'] and '.nersc.gov' in i['_source']['dest']:
+                                print('EW traffic DTNs')
+                                total_ew += float(i['_source']['values']['file_size_MB'])
+                            elif '.nersc.gov' in i['_source']['source'] and '.nersc.gov' not in i['_source']['dest']:
+                                print('NS traffic-leaving NERSC')
+                                total_out_ns += float(i['_source']['values']['file_size_MB'])
+                                if (50000 <= int(i['_source']['meta']['src_port']) <= 51000 and 
+                                    50000 <= int(i['_source']['meta']['dst_port']) <= 51000):
+                                    print('This is a globus transfer')
+                                    total_size_globus_out += float(i['_source']['values']['file_size_MB'])
+                            elif '.nersc.gov' in i['_source']['dest'] and '.nersc.gov' not in i['_source']['source']:
+                                print('NS traffic-coming NERSC in 2022')
+                                total_in_ns += float(i['_source']['values']['file_size_MB'])
                             if (50000 <= int(i['_source']['meta']['src_port']) <= 51000 and 
                                 50000 <= int(i['_source']['meta']['dst_port']) <= 51000):
-                                print('This is a globus transfer')
-                                total_size_globus_out += float(i['_source']['values']['file_size_MB'])
-                        elif '.nersc.gov' in i['_source']['dest'] and '.nersc.gov' not in i['_source']['source']:
-                            print('NS traffic-coming NERSC')
-                            total_in_ns += float(i['_source']['values']['file_size_MB'])
-                            if (50000 <= int(i['_source']['meta']['src_port']) <= 51000 and 
-                                50000 <= int(i['_source']['meta']['dst_port']) <= 51000):
-                                print('This is a globus transfer')
+                                print('This is a globus transfer from 2022')
                                 total_size_globus_in += float(i['_source']['values']['file_size_MB'])
 
     print(total_ew)
